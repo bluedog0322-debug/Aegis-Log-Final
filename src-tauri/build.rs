422e35a2f10b12c1ff1fab8e24cfef1eb95e8ba0
bluedog0322-debug <bluedog.0322@gmail.com>
2026-01-06 23:12:45 +0900

    Update build.rs

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,8 +1,3 @@
 fn main() {
-    // アイコンの自動検索を完全にスキップする設定です
-    let attrs = tauri_build::Attributes::new()
-        .windows_attributes(tauri_build::WindowsAttributes::new());
-    
-    // 標準のビルドではなく、属性を指定したビルドを行います
-    tauri_build::build_with_attributes(attrs);
+  tauri_build::build()
 }
